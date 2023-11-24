094df5556f64002e20acb3a1b8d820dab5312f54
Amyn00 <medamine190@gmail.com>
2023-11-24 06:12:11 -0800

    update

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -2,11 +2,11 @@
 
 package { 'flask':
   ensure   => '2.1.0',
-  provider => 'pip',
+  provider => 'pip3',
 }
 
 # Ensure Werkzeug version 2.1.1
 package { 'Werkzeug':
   ensure   => '2.1.1',
-  provider => 'pip',
+  provider => 'pip3',
 }

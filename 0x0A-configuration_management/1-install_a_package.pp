63d2003370fc59bb86f0c49d14451c73f1fb977c
Amyn00 <medamine190@gmail.com>
2023-11-24 06:03:41 -0800

    update

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,13 +1,6 @@
-#Install python3-pip package.
-
-package { 'python3-pip':
-  ensure => installed,
-}
-
 # Install flask version 2.1.0 using pip3.
 
 package { 'flask':
   ensure   => '2.1.0',
   provider => 'pip',
-  require  => Package['python3-pip'],
 }
